7c21bc68357ee28f763669b9d8417642b316ce1b
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-11-29 18:06:15 +0400

    Add files via upload

---
+++ AdapterDesignPattern_Example_Task/AdapterDesignPattern_Example_Task/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Square
+{
+    private double side;
+
+    public Square(double side)
+    {
+        this.side = side;
+    }
+
+    public double CalculateArea()
+    {
+        return side * side;
+    }
+}
+
+public interface IShape
+{
+    double GetArea();
+}
+
+public class SquareAdapter : IShape
+{
+    private Square square;
+
+    public SquareAdapter(double side)
+    {
+        this.square = new Square(side);
+    }
+
+    public double GetArea()
+    {
+        return square.CalculateArea();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IShape squareAdapter = new SquareAdapter(5.0);
+        Console.WriteLine("Area: " + squareAdapter.GetArea());
+    }
+}
